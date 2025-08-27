6d53bb4a69bcd9fbe82ba751f25482bf8dc9d695
unknown <wotlr6894@naver.com>
2025-08-27 15:52:11 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wellcome in Jacode-Blog
+          Wellcome to Jacode-Blog
         </p>
         <a
           className="App-link"
