88c617a26b9109f06a4548e2e757f82a2dd44b23
unknown <wotlr6894@naver.com>
2025-08-27 17:52:55 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Wellcome to Jacode-Blog</p>
+        <p>Wellcome to Jacode-Blog22</p>
         <a
           className="App-link"
           href="https://reactjs.org"
