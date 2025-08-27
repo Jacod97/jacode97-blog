c778808d54c621384193a515214a94a787878313
unknown <wotlr6894@naver.com>
2025-08-27 20:18:18 +0900

    mkdir component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import Header from './components/Header';
 import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Hero />
-      <Projects />
-      <Contact />
+      <MainLayout />
     </div>
   );
 }
