f5b5d69e5aad9fa1e5fd5af5551b86e09056f487
unknown <wotlr6894@naver.com>
2025-09-13 12:58:13 +0900

    tetst

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -43,7 +43,7 @@ export default function ChatBox({ theme }) {
     setInput("");
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/ask", {
+      const response = await fetch("https://blog-chatbot-1o2k.onrender.com/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ session_id: sessionId, question })
